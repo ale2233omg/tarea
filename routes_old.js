33d35cd64730b6788a5ea67e1c6b63d1a0d6b3c1
Guille Zorron <gzorron@xmartlabs.com>
2023-09-06 20:21:41 -0300

    remove ruido

--- routes_old.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getMessage } from './controllers/messages-controller.js';
-
-
-const router = Router();
-
-router.get('/message/:message', getMessage)
-
-export { router };
