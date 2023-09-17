388829e2ada17794cbe238ca1610aae2c7417c90
unknown <fernando.escobal@anima.edu.uy>
2023-09-17 00:49:41 -0300

    a

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+
+export const secretKey = 'tu_clave_secreta';
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import { router } from './routes.js';
+import jwt from 'jsonwebtoken';
+import { secretKey } from './config.js';
+
 const app = express();
 
-app.use('/', router)
+app.use('/', router);
 
 app.listen(3000, () => {
-  console.log('server ATR')
-})
+  console.log('server ATR');
+});
+
+// Función para crear un token JWT
+const createToken = (payload) => {
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+};
+
+// Función para verificar y decodificar un token JWT
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return decoded;
+  } catch (error) {
+    throw new Error('Token inválido');
+  }
+};
+
+export { createToken, verifyToken };
